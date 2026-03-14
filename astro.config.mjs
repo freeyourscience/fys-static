0cec8e4c683b7dab817be1839ac30612cce205d6
Lukas Grossberger <code@grossberger.xyz>
2026-03-14 17:38:48 +0100

    add missing base paths for pages and trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://freeyourscience.github.io',
-  base: '/fys-static',
+  base: '/fys-static/',
 });
