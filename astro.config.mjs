8dac51aa3af127deca5d4785ae52ef66290c7369
Lukas Grossberger <code@grossberger.xyz>
2026-03-14 17:57:05 +0100

    update astro config to freeyourscience.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://freeyourscience.github.io',
-  base: '/fys-static/',
+  site: 'http://freeyourscience.org/',
+  base: '/',
 });
